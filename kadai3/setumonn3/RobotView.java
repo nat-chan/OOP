73731abef9dc058e49592ab29da4ee4a8dcc0139
nat-chan <qzrp01@gmail.com>
2018-01-24 09:39:06 +0900

    elect氏の成果をマージ

--- kadai3/setumonn3/RobotView.java
+++
@@ -1,5 +0,0 @@
-interface RobotView {
-    public void turn(double oldHeading, double newHeading);
-    public void move(double oldX, double oldY,
-                     double newX, double newY);
-}
