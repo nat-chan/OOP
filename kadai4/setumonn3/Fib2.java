47165c7734b1ccc8c3b8b4172c3138390421fcbc
elect <e.tmailbank@gmail.com>
2018-01-31 10:02:30 +0900

    Update Fib2.java

--- kadai4/setumonn3/Fib2.java
+++ kadai4/setumonn3/Fib2.java
@@ -3,25 +3,36 @@ import java.util.*;
 public class Fib2
 {
     static int i;
-    static Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+    // フィボナッチの計結果を保存していくためのマップ
+    // <n, fib(n)>
+    static Map<Integer, Integer> map = new HashMap<Integer,Integer>(); 
+    
+    /**
+    * フィボナッチを計算するメソッド
+    * @param n
+    * @return fib(n)
+    **/    
     static int fib(int n) {
         if (n < 2) {
-            return 1;
+            return 1; // fib(1), fib(0) ならば 1 を返す
         } else if (map.containsKey(n)){
-            return map.get(n);
+            return map.get(n); // fib(n) が既に計算済みならばそれを返す
         }
         else {
-            i = (fib(n-1) + fib(n - 2)) % 100000;
-            map.put(n, i);
-            return i % 100000;
+            i = (fib(n-1) + fib(n - 2)) % 100000; // 下5桁のみを取り出す
+            map.put(n, i); // 取り出した計算結果を保存する
+            return i; // 計算結果を返す
         }
     }
-
+    /**
+    * フィボナッチの計算時間を計測するためのメソッド
+    * @param args[0] nの数
+    **/
     public static void main(String args[])
     {
-        long start = System.nanoTime();
+        long start = System.nanoTime(); // 現在時間を保存
         int n = Integer.parseInt(args[0]);
-        System.out.println("fib(" + n + ") = " + fib(n));
-        System.out.println((System.nanoTime() - start) / 1000000 + "ms");
+        System.out.println("fib(" + n + ") = " + fib(n)); // fib(n) を計算
+        System.out.println((System.nanoTime() - start) / 1000000 + "ms"); // 計算終了時の時間と現在時間を減算する
     }
 }
