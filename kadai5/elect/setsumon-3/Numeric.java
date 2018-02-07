dfaaf3e879b5d5d16fb3d398bf36c08fa9c0a1e3
MokkeMeguru <meguru.mokke@gmail.com>
2018-02-08 01:56:56 +0900

    setsumon-3-finish

---
+++ kadai5/elect/setsumon-3/Numeric.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public interface Numeric<E extends Numeric> {
+    public E add(E num);
+    public E subtract(E num);
+    public E multiply(E num);
+    public E divide(E num);
+    public String toString();
+}
